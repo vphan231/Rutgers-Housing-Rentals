6520de4e1e3bf754a6c73d76bc3ec0d640b318ba
Vincent Phan <vincent.phan@rutgers.edu>
2019-12-07 00:14:47 -0500

    login.jsx

--- react-backend/client/src/pages/login.jsx
+++ react-backend/client/src/pages/login.jsx
@@ -6,12 +6,37 @@ import {
     Link,
     useParams
   } from "react-router-dom";
+  import {Form, Button} from 'react-bootstrap'
 
 
 const Login = () => {
 
     return (
-        <div>Login below</div>
+    <div >
+        <br />
+        <Form>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group>
+                <Link to='/register'>Do not have an account? Please register here.</Link>
+            </Form.Group>
+            
+            <Button variant="primary" type="submit">
+                Login
+            </Button>
+        </Form>
+        <br />
+    </div>
     );
 }
 

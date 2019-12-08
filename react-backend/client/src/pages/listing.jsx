80deafa75936d05e3d2f0063327b382d5412d9ab
Vincent Phan <vincent.phan@rutgers.edu>
2019-12-07 21:49:25 -0500

    small change listing

--- react-backend/client/src/pages/listing.jsx
+++ react-backend/client/src/pages/listing.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import {
     BrowserRouter as Router,
     Switch,
@@ -9,10 +10,22 @@ import {
 
 
 const Listing = () => {
+    //this.state = { title: "", address: "", price: "", Max_Occupancy: "", Has_Driveway: "", Is_Available: "", imageSrc: "" };
+
     let { id } = useParams();
 
+    axios.get('/listing/'+id)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+    });
+
     return (
-        <div>Listing id: {id}</div>
+        <div>
+            Listing id: {id}
+        </div>
     );
 }
 

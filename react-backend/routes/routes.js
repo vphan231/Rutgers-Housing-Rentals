706868d2a4ea82b759375813e3ffdf1002234c07
Colin Ackerley <colinackerley14@gmail.com>
2019-12-07 19:26:31 -0500

    Added basic input validation

--- react-backend/routes/routes.js
+++ react-backend/routes/routes.js
@@ -2,29 +2,34 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 const User = require("../models/User");
+const { check, validationResult } = require('express-validator');
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
-
 /* POST register */
-router.post("/register", (req, res) => {
-  console.log("Received post request", req.body);
-
+router.post("/register", [
+  check('name').isLength({ min: 3 }),
+  check('email').isEmail(),
+  check('phone').isMobilePhone().withMessage('Please enter a valid phone number'),
+  check('password').isString().isLength({min:5, max:15}).withMessage('Please enter a password between 5 and 15 characters')
+], (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() })
+  }
   const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
     phone: req.body.phone
-  });
-
+  })
   //console.log(user);
   user.save(error => {
-    if (error) {
+    if (error || !errors.isEmpty()) {
       console.log("Error saving user");
       res.send(error);
     } else {
@@ -33,5 +38,4 @@ router.post("/register", (req, res) => {
     }
   });
 });
-
 module.exports = router;
